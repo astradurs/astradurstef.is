457747b429b8a53fd37754e3cd87e4f12f0ac415
Ástráður Stefánsson <astradur@maul.is>
2023-12-22 14:32:30 +0000

    removed the prize component

--- my-site/astradurstef.is/src/app/projects/games/gudruns-xmas-wordle/components/the-prize.tsx
+++
@@ -1,8 +0,0 @@
-export default async function ThePrize() {
-  return (
-    <div>
-      <h1>YOU HAVE WON A TICKET TO PRIMAVERA PORTUGAL</h1>
-      <h2>CONGRATULATIONS AND MERRY CHRISTMAS</h2>
-    </div>
-  )
-}
