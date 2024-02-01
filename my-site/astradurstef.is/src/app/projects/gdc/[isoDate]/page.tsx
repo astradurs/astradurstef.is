1ce90959f66787938ccd0b1d3b0863429b781717
Ástráður Stefánsson <astradur@maul.is>
2024-02-01 18:15:42 +0000

    added time to gdcEvent

--- my-site/astradurstef.is/src/app/projects/gdc/[isoDate]/page.tsx
+++ my-site/astradurstef.is/src/app/projects/gdc/[isoDate]/page.tsx
@@ -5,6 +5,7 @@ import { eventQuery } from "@/lib/sanity/lib/queries"
 import { sanityFetch } from "@/lib/sanity/lib/fetch"
 import { PortableText, PortableTextComponents } from "@portabletext/react"
 import GDCWaitlist from "./components/gdc-waitlist"
+import EventDescription from "./components/event-description"
 
 export default async function GDCEvent({
   params,
@@ -22,7 +23,7 @@ export default async function GDCEvent({
   const event: {
     title: string
     body: any
-    date: string
+    date: Date
     location: {
       title: string
       address: string
@@ -45,72 +46,6 @@ export default async function GDCEvent({
           isoDate={params.isoDate}
           name={authUser.firstName || "no name ?!"}
         />
-      </div>
-    </div>
-  )
-}
-
-const components: PortableTextComponents = {
-  block: ({ children }) => {
-    return <p>{children}</p>
-  },
-  marks: {
-    b: ({ children }) => <span className="font-semibold">{children}</span>,
-  },
-}
-
-async function EventDescription({
-  event,
-}: {
-  event: {
-    title: string
-    body: any
-    date: string
-    location: {
-      title: string
-      address: string
-    }
-    limit: number
-  }
-}) {
-  const eventDate = new Date(event.date)
-  const eventDateFormatted = eventDate.toLocaleDateString("is-IS", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })
-
-  const locationQuery =
-    event.location.title.split(" ").join("+") +
-    "+" +
-    event.location.address.split(" ").join("+")
-  return (
-    <div className="grid gap-4 sm:px-4">
-      <h1 className="font-bold text-xl">{event.title}</h1>
-      <div className="grid gap-2">
-        <PortableText value={event.body} components={components} />
-      </div>
-      <div className="grid">
-        <span className="font-bold text-xl">HVAR?!</span>
-        <a
-          href={`https://maps.google.com/?q=${locationQuery}`}
-          className="grid group"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="font-semibold group-hover:text-primary/70">
-            {event.location.title}
-          </span>
-          <span className="text-sm text-primary/70 group-hover:text-primary/40">
-            {event.location.address}
-          </span>
-        </a>
-      </div>
-      <div className="grid">
-        <span className="font-bold">HVENÆR?!</span>
-        <div className="flex gap-1">
-          <span>{eventDateFormatted}</span>
-        </div>
       </div>
     </div>
   )
